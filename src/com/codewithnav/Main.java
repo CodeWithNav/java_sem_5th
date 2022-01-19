462ff34661ea680b3352d7a5d56f066d41706a13
Navpreet Singh <navpreetsingh951818@gmail.com>
2022-01-19 17:56:01 +0530

    Method Overloading

--- src/com/codewithnav/Main.java
+++ src/com/codewithnav/Main.java
@@ -1,11 +1,20 @@
+/*
+* Name : Navpreet Singh
+* Aim : Java Method Overloading
+* Date : 15/12/2021
+* */
 package com.codewithnav;
-
-import com.codewithnav.models.Person;
-import com.codewithnav.models.Student;
-
 public class Main {
     public static void main(String[] args) {
-        Person person = new Student("Navpreet Singh");
-        ((Student) person).study();
+        Main m1 = new Main();
+
+        System.out.println("Int Sum : "+m1.sum(4,3));
+        System.out.println("Double Sum : "+m1.sum(4.2,3.5));
+    }
+   public int sum(int a ,int b){
+        return a+b;
+   }
+    public double sum(double a ,double b){
+        return a+b;
     }
 }
