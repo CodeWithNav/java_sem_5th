73a32d09fd82bfc3dbd744269b3403ed36d481e6
Navpreet Singh <navpreetsingh951818@gmail.com>
2022-01-19 17:57:57 +0530

    init

--- src/com/codewithnav/Main.java
+++ src/com/codewithnav/Main.java
@@ -1,11 +1,8 @@
 package com.codewithnav;
 
-import com.codewithnav.models.Person;
-import com.codewithnav.models.Student;
 
 public class Main {
     public static void main(String[] args) {
-        Person person = new Student("Navpreet Singh");
-        ((Student) person).study();
+
     }
 }
